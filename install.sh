d7732aee2fa2078099d344cf50ab7ab9b821bf51
Fabio Scala <fabio.scala@gmail.com>
2014-12-14 19:52:55 +0100

    add comments

--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+# Current script dir
 cd "$(dirname "$0")"
 
+# Install required tools (node.js, npm, yeoman, bower and grunt)
 sudo apt-get update
 sudo apt-get install nodejs nodejs-legacy npm -y
 sudo npm install -g npm@latest
 sudo npm install -g yo bower grunt-cli
 
-# npm sometimes causes permission issues after installing things globally
+# Fix premission issues that npm sometimes causes installing things globally
 sudo npm cache clear
 sudo chown -R `whoami` ~/tmp
 
+# Install packages from packages.json
 npm install
 
-# don't fail just in case someone is really running this as root
-# analytics: don't prompt for usage stats
+# Install all JavaScript (browser) modules required by the appication
+# alow-root: Don't fail just in case someone is really running this as root
+# config.analytics: Don't prompt for usage stats
 bower install --config.analytics=false --allow-root
